5a292cfdf4d0abe2396ded5aee8297f6007f72e2
T.NATION <tonyenation430@gmail.com>
2024-09-14 00:08:47 +0100

    Turned the more product page into a component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Picture from "../components/picture"
 import Istcard from "../components/istcard"
 import Sidekick from "@/components/sidekick";
+import Testimonials from "@/components/testimonials";
+import Moreproduct from "@/components/moreproduct";
 
 export default function Home() {
   return (
@@ -191,6 +193,8 @@ export default function Home() {
             {/* sidekick page */}
 
              < Sidekick />
+
+             
               {/* border div */}
             <div className="container mx-auto w-full h-full  rounded-full -mt-8 bg-white">
                 <div className=" rounded-full py-6 border-t-2 border-gray-400"></div>
@@ -198,59 +202,9 @@ export default function Home() {
 
 
             {/* testimonial cards */}
-
-            <div className="container mx-auto my-6">
-              <section className="text-gray-900 body-font">
-                <div className="container px-24 mx-auto ">
-                    <div className="mx-auto max-w-screen-2xl px- md:px-8">
-                   {/* first testimonial */}
-                        <div className="grid grid-cols-6 gap-4 flex-wrap md:gap-6">
-                      <div className="relative col-span-2 bg-gray-100 rounded-lg">
-                      <img className=" w-full h-52 bg-contain rounded-lg object-cover" src="images/man.jpg" alt="Profile Image"/>
-  
-                      </div>
-
-                      <div className="relative col-span-4 ">
-                      <div className="bg-gray-100 p-4 border-2 border-gray-300 rounded-lg ">
-          <p className="mb-6 text-base">"The customer service team went above and beyond to assist me with my issue. They were responsive, friendly. The customer service
-          team went above and beyond to assist my issue. They were responsive, friendly".</p>
-            <span className="flex-grow flex flex-col pl-4">
-              <span className="font-bold text-lg text-gray-900">Joe Campbell</span>
-              <span className="text-gray-500 text-xs">Boston USA</span>
-            </span>
-
-        </div>
-                      </div>
-
-                    </div>
-
-
-
-                    {/* second testimonial */}
-
-                    <div className="grid grid-cols-6 gap-4 flex-wrap mt-8 md:gap-6">
-                    <div className="relative col-span-4 ">
-                      <div className="bg-gray-100 p-4 border-2 border-gray-300 rounded-lg ">
-          <p className="mb-6 text-base">"The customer service team went above and beyond to assist me with my issue. They were responsive, friendly. The customer service
-          team went above and beyond to assist my issue. They were responsive, friendly".</p>
-            <span className="flex-grow flex flex-col pl-4">
-              <span className="font-bold text-lg text-gray-900">Adam Zampa</span>
-              <span className="text-gray-500 text-xs">Founder</span>
-            </span>
-
-        </div>
-                      </div>
-                      <div className="relative col-span-2 bg-gray-100 rounded-lg">
-                      <img className=" w-full h-52 bg-cover rounded-lg object-fit" src="images/woman.jpg" alt="Profile Image"/>
-  
-                      </div>
-                    </div>
-                    </div>
-                </div>
-              </section>
-
-            </div>
-
+              
+              <Testimonials />
+             
              {/* end of testimonials */}
 
 
@@ -423,122 +377,7 @@ export default function Home() {
         </div>
 
           {/* more abt */}
-          <div className="container mx-auto">
-                <h2 className="font-bold mt-14">
-                  More by Oyasim Ahmed
-          </h2>
-
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 py-2 md:py-6 w-full">
-  <div className="flex item-center  h-fit relative overflow-hidden group ">
-    <a className="" href="">
-   <img src="https://cdn.dribbble.com/userupload/15524891/file/original-a30aa1c36729b7bffaa65244990b4348.png?resize=400x300&amp;vertical=center" sizes="263px"/>
-    <li className="list-none w-full absolute  rounded-2xl group-hover:-bottom-1  bg-gradient-to-t from-gray-900 via-gray-900/40 bg-opacity-50 px-6 py-2 flex ">
-    <p className="text-white pt-3  font-semibold">Finance Webs...</p>
-    <div className="flex my-2 ml-4 space-x-3">
-      <a className="">
-         <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
-            <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-               </path>
-             </svg>
-              </button>
-                  </a>
-                  <a className="">
-                    <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
-                        <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                      </svg>
-                    </button>
-                  </a>
-                  </div>
-                   </li>
-                 </a>
-  </div>
-
-  <div className="flex item-center h-fit relative overflow-hidden group ">
-    <a className="" href="">
-   <img src="https://cdn.dribbble.com/userupload/10394304/file/original-0de9df7e96da55a0aa9ab534f85a58c6.jpg?resize=400x0" className="w-full"/>
-    <li className="absolute w-full  rounded-2xl  group-hover:-bottom-1  bg-gradient-to-t from-gray-900 via-gray-900/40 bg-opacity-50 px-6 py-4 flex ">
-    <p className="text-white font-semibold ">Dashbord De...</p>
-    <div className="flex mx-2 ml-8 space-x-3">
-      <a className="">
-                    <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
-                     <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                     </path>
-                   </svg>
-                    </button>
-                  </a>
-                  <a className="">
-                    <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
-                        <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                      </svg>
-                    </button>
-                  </a>
-    </div>
-    </li>
-    </a>
-	</div>
-
-  <div className="flex item-center h-fit relative overflow-hidden group ">
-    <a className="" href="">
-  <img  src="https://cdn.dribbble.com/userupload/7872535/file/original-8d5500fc9284352e22aab524e166ba3e.jpg?resize=400x300&amp;vertical=center" alt="" />
-    <li className="absolute w-full  rounded-2xl group-hover:-bottom-1  bg-gradient-to-t from-gray-900 via-gray-900/40 bg-opacity-50 px-6 py-4 flex  ">
-    <p className="text-white font-semibold ">Grocery App...</p>
-    <div className="flex ml-6 space-x-3 ">
-      <a className="" href="">
-      <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
-     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
-    <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-  </path>
-  </svg>
-     </button>
-      </a>
-    <a className="" href="">
-     <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
-           <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-           </svg>
-                    </button>
-                  </a>
-                  </div>
-    </li>
-    </a>
-	</div>
-
-  <div className="bg-gray flex item-center h-fit relative overflow-hidden group ">
-    <a className="" href="">
-  <img src="https://cdn.dribbble.com/userupload/10204559/file/original-9ac46e059940ea8bad8dc7ece93467df.jpg?resize=400x300&amp;vertical=center" sizes="263px" 
-   alt="" />
-    <li className="absolute w-full rounded-2xl group-hover:-bottom-1  bg-gradient-to-t from-gray-900 via-gray-900/40 bg-opacity-50 px-8 p-2 flex ">
-    <p className="text-white font-semibold pt-3 ">Dashbord De...</p>
-    <div className="flex my-2 ml-6 space-x-3 ">
-     <a className="" href="">
-      <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
-     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
-    <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-  </path>
-  </svg>
-     </button>
-      </a>
-       <a className="" href="">
-     <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
-           <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-           </svg>
-                    </button>
-     </a>
-     </div>
-    </li>
-    </a>
-	</div>
-
-
-</div>
-
-         </div>
-
+         <Moreproduct />
 
 
                 {/*like page  */}
---
+++ components/moreproduct.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+
+const Moreproduct = () => {
+    return (
+        <main className="mx-auto  relative bg-white ">
+        <div className="container mx-auto">
+                <h2 className="font-bold mt-14">
+                  More by Oyasim Ahmed
+          </h2>
+
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 py-2 md:py-6 w-full">
+  <div className="flex item-center  h-fit relative overflow-hidden group ">
+    <a className="" href="">
+   <img src="https://cdn.dribbble.com/userupload/15524891/file/original-a30aa1c36729b7bffaa65244990b4348.png?resize=400x300&amp;vertical=center" sizes="263px"/>
+    <li className="list-none w-full absolute  rounded-2xl group-hover:-bottom-1  bg-gradient-to-t from-gray-900 via-gray-900/40 bg-opacity-50 px-6 py-2 flex ">
+    <p className="text-white pt-3  font-semibold">Finance Webs...</p>
+    <div className="flex my-2 ml-4 space-x-3">
+      <a className="">
+         <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
+            <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+               </path>
+             </svg>
+              </button>
+                  </a>
+                  <a className="">
+                    <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
+                        <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                    </button>
+                  </a>
+                  </div>
+                   </li>
+                 </a>
+  </div>
+
+  <div className="flex item-center h-fit relative overflow-hidden group ">
+    <a className="" href="">
+   <img src="https://cdn.dribbble.com/userupload/10394304/file/original-0de9df7e96da55a0aa9ab534f85a58c6.jpg?resize=400x0" className="w-full"/>
+    <li className="absolute w-full  rounded-2xl  group-hover:-bottom-1  bg-gradient-to-t from-gray-900 via-gray-900/40 bg-opacity-50 px-6 py-4 flex ">
+    <p className="text-white font-semibold ">Dashbord De...</p>
+    <div className="flex mx-2 ml-8 space-x-3">
+      <a className="">
+                    <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
+                     <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                     </path>
+                   </svg>
+                    </button>
+                  </a>
+                  <a className="">
+                    <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
+                        <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                    </button>
+                  </a>
+    </div>
+    </li>
+    </a>
+	</div>
+
+  <div className="flex item-center h-fit relative overflow-hidden group ">
+    <a className="" href="">
+  <img  src="https://cdn.dribbble.com/userupload/7872535/file/original-8d5500fc9284352e22aab524e166ba3e.jpg?resize=400x300&amp;vertical=center" alt="" />
+    <li className="absolute w-full  rounded-2xl group-hover:-bottom-1  bg-gradient-to-t from-gray-900 via-gray-900/40 bg-opacity-50 px-6 py-4 flex  ">
+    <p className="text-white font-semibold ">Grocery App...</p>
+    <div className="flex ml-6 space-x-3 ">
+      <a className="" href="">
+      <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
+     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
+    <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+  </path>
+  </svg>
+     </button>
+      </a>
+    <a className="" href="">
+     <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
+           <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+           </svg>
+                    </button>
+                  </a>
+                  </div>
+    </li>
+    </a>
+	</div>
+
+  <div className="bg-gray flex item-center h-fit relative overflow-hidden group ">
+    <a className="" href="">
+  <img src="https://cdn.dribbble.com/userupload/10204559/file/original-9ac46e059940ea8bad8dc7ece93467df.jpg?resize=400x300&amp;vertical=center" sizes="263px" 
+   alt="" />
+    <li className="absolute w-full rounded-2xl group-hover:-bottom-1  bg-gradient-to-t from-gray-900 via-gray-900/40 bg-opacity-50 px-8 p-2 flex ">
+    <p className="text-white font-semibold pt-3 ">Dashbord De...</p>
+    <div className="flex my-2 ml-6 space-x-3 ">
+     <a className="" href="">
+      <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
+     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
+    <path d="M3.33337 5.2C3.33337 4.0799 3.33337 3.51984 3.55136 3.09202C3.74311 2.71569 4.04907 2.40973 4.42539 2.21799C4.85322 2 5.41327 2 6.53337 2H9.46671C10.5868 2 11.1469 2 11.5747 2.21799C11.951 2.40973 12.257 2.71569 12.4487 3.09202C12.6667 3.51984 12.6667 4.0799 12.6667 5.2V14L8.00004 11.3333L3.33337 14V5.2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+  </path>
+  </svg>
+     </button>
+      </a>
+       <a className="" href="">
+     <button className="w-full px-2 py-2 text-base rounded-full text-center bg-white">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" className="icon ">
+           <path d="M10.7408 2C13.0889 2 14.6667 4.235 14.6667 6.32C14.6667 10.5425 8.11856 14 8.00004 14C7.88152 14 1.33337 10.5425 1.33337 6.32C1.33337 4.235 2.91115 2 5.2593 2C6.60745 2 7.48893 2.6825 8.00004 3.2825C8.51115 2.6825 9.39263 2 10.7408 2Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+           </svg>
+                    </button>
+     </a>
+     </div>
+    </li>
+    </a>
+	</div>
+
+
+</div>
+
+         </div>
+
+      </main>
+       
+    )
+}
+
+export default Moreproduct
