69dddd76400e92a2525f68a45becad61a1aed405
T.NATION <tonyenation430@gmail.com>
2024-09-13 23:51:49 +0100

    Turn the sidekick page into a compoent

--- components/istcard.tsx
+++ components/istcard.tsx
@@ -69,8 +69,8 @@ const Istcard = () => {
                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
                   </svg>
 
-   </span>
-  </p>
+                  </span>
+                        </p>
                         <h3 className="text-xl pb-4 font-bold text-gray-800 ">Learning Hub</h3>
                         <p className="mb-2 text-xs text-gray-900">Recieved actionable recommendations
                           based on your financial behaviour.</p>
