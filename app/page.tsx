cf717c13c7fa856f21e005bf31454c089d2828ff
T.NATION <tonyenation430@gmail.com>
2024-09-13 23:47:05 +0100

    turn the ist card into a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Picture from "../components/picture"
+import Istcard from "../components/istcard"
 
 export default function Home() {
   return (
@@ -84,93 +85,11 @@ export default function Home() {
             </div>
 
             
-            <Picture />
+             <Picture />
 
             {/*1st cards */}
-            <div className="container mx-auto flex flex-wrap py-16 flex-col md:flex-row items-center">
-              <section className="text-gray-900 body-font">
-                <div className="container px-5 py-23 mx-auto ">
-                  <div className=" w-full px-28 mx-auto">
-                    <h1 className=" font-bold text-4xl text-center">Everyday Personal Finance, Tailored to Meet Your Aspirations</h1>
-                    <div className="grid grid-cols-3 gap-4 flex-wrap mt-16 text-center">
-                      <div className="relative p-5 bg-gray-100 border-2 border-gray-300 rounded-lg">
-                      <p className="flex flex-col items-center text-center cursor-pointer mb-4">
-                    <span className="rounded-full border border-gray-500 py-2 px-1.5 h-10 w-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.098 19.902a3.75 3.75 0 0 0 5.304 0l6.401-6.402M6.75 21A3.75 3.75 0 0 1 3 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 0 0 3.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008Z" />
-                        </svg>
-                       </span>
-                       </p>
-        
-                        <h3 className="text-xl pb-4 font-bold text-gray-800 ">Smart Insight</h3>
-                        <p className="mb-2 text-xs text-gray-900">Recieved actionable recommendations                     based on your financial behaviour.</p>
-                      </div>
-
-                      <div className="relative h-full p-5 bg-gray-100 border-2 border-gray-300 rounded-lg">
-                      <p className="flex flex-col items-center text-center cursor-pointer mb-4">
-                    <span className="rounded-full border border-gray-500 py-2 px-1.5 h-10 w-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.098 19.902a3.75 3.75 0 0 0 5.304 0l6.401-6.402M6.75 21A3.75 3.75 0 0 1 3 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 0 0 3.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008Z" />
-                        </svg>
-                         </span>
-                           </p>
-                        <h3 className="text-xl pb-4 font-bold text-gray-800 ">Budget Buddy</h3>
-                        <p className="mb-2 text-xs text-gray-900">Recieved actionable recommendations
-                          based on your financial behaviour.</p>
-                      </div>
-
-                      <div className="relative h-full p-5 bg-gray-100 border-2 border-gray-300 rounded-lg">
-                      <p className="flex flex-col items-center text-center cursor-pointer mb-4">
-                    <span className="rounded-full border border-gray-500 py-2 px-1.5 h-10 w-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.098 19.902a3.75 3.75 0 0 0 5.304 0l6.401-6.402M6.75 21A3.75 3.75 0 0 1 3 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 0 0 3.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008Z" />
-                        </svg>
-                   </span>
-                      </p>
-                        <h3 className="text-xl pb-4 font-bold text-gray-800 ">Growth Forecaster</h3>
-                        <p className="mb-2 text-xs text-gray-900">Recieved actionable recommendations
-                          based on your financial behaviour.</p>
-                      </div>
-                    </div>
-                    <div className="grid grid-cols-5 gap-4 flex-wrap mt-8 text-center">
-                      <div className="relative col-span-2 p-5 bg-gray-100 border-2 border-gray-300 rounded-lg ">
-                        <p className="flex flex-col items-center text-center cursor-pointer mb-4">
-
-                          <span className="rounded-full border border-gray-500 py-2 px-1.5 h-10 w-10">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
-                             </svg>
-                             </span>
-                            </p>
-                        <h3 className="text-xl pb-4 font-bold text-gray-800 ">Security Shield</h3>
-                        <p className="mb-2 text-xs text-gray-900">Recieved actionable recommendations based on your financial behaviour.</p>
-                      </div>
-
-                      <div className="relative col-span-3 p-5 bg-gray-100 border-2 border-gray-300 rounded-lg">
-                        <p className="flex flex-col items-center text-center cursor-pointer mb-4">
-                    <span className="rounded-full border border-gray-500 py-2 px-1.5 h-10 w-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
-</svg>
-
-   </span>
-  </p>
-                        <h3 className="text-xl pb-4 font-bold text-gray-800 ">Learning Hub</h3>
-                        <p className="mb-2 text-xs text-gray-900">Recieved actionable recommendations
-                          based on your financial behaviour.</p>
-                      </div>
-
-                    </div>
-
-
-                  </div>
-
-
-                </div>
-              </section>
-
-            </div>
-
+            
+              <Istcard />
             {/* another card */}
 
             <div className="container mx-auto flex flex-wrap py-16 flex-col md:flex-row items-center">
@@ -368,7 +287,7 @@ export default function Home() {
 
               {/* border div */}
             <div className="container mx-auto w-full h-full  rounded-full -mt-8 bg-white">
-<div className=" rounded-full py-6 border-t-2 border-gray-400"></div>
+                <div className=" rounded-full py-6 border-t-2 border-gray-400"></div>
                  </div>
 
 
@@ -736,3 +655,5 @@ export default function Home() {
 
   );
 }
+
+
